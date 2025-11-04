c5b4aeb6eb2ccacb7d02ff1cdf633c1b640719c8
Elimelech Ehrlich <ee525566@gmail.com>
2025-11-04 15:01:03 +0200

    fixed order class

--- order.py
+++ order.py
@@ -1,14 +1,19 @@
 class Order:
-    def __init__(self, items, total_price):
+    def __init__(self, items:list[str], total_price:float):
         self.items = items
         self.total_price = total_price
-
-    def print_invoice(self):
-        print (f'{self.items}: {self.total_price}')
 
 class InvoicePrinter:
     def __init__(self):
         pass
     @staticmethod
     def print_invoice(order: Order):
-        print (f'{order.items}: {order.total_price}')
+        Item_number = 1
+        for item in order.items:
+            print (f'{Item_number}: {item}')
+            Item_number += 1
+        print (f'Total price: {order.total_price}')
+
+a = Order(['milk', 'bread', 'shampoo'], 30)
+b = InvoicePrinter().print_invoice(a)
+b.print_invoice(a)
